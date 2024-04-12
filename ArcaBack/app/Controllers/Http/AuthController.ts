78c2d8f0ac1801d66727083dac1548072f1c34d2
adrienmorin1 <adridu71170@gmail.com>
2024-04-12 15:25:18 +0200

    first steps to implement back end

---
+++ ArcaBack/app/Controllers/Http/AuthController.ts
@@ -0,0 +1,11 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+
+  public async register(){
+    return this.register.name
+  }
+  public async login(){
+    return this.login.name
+  }
+}
