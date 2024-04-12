6f44e2a97e3aef864fc23e8022961edecf5c32a6
Gr1Gny <97124361+Gr1Gny@users.noreply.github.com>
2024-04-12 10:56:51 +0200

    ca marche pitié

--- nuxt-ui-pro-dashboard/pages/index.vue
+++ nuxt-ui-pro-dashboard/pages/index.vue
@@ -3,39 +3,13 @@ import { sub } from 'date-fns'
 import type { Period, Range } from '~/types'
 import LoginForm from "~/components/users/LoginForm.vue";
 
-const { isNotificationsSlideoverOpen } = useDashboard()
-
-const items = [[{
-  label: 'New mail',
-  icon: 'i-heroicons-paper-airplane',
-  to: '/inbox'
-}, {
-  label: 'New user',
-  icon: 'i-heroicons-user-plus',
-  to: '/users'
-}]]
-
-const range = ref<Range>({ start: sub(new Date(), { days: 14 }), end: new Date() })
-const period = ref<Period>('daily')
 </script>
 
 <template>
   <UDashboardPage>
     <UDashboardPanel grow>
       <UDashboardNavbar title="Home">
-        <template #right>
-          <UTooltip text="Notifications" :shortcuts="['N']">
-            <UButton color="gray" variant="ghost" square @click="isNotificationsSlideoverOpen = true">
-              <UChip color="red" inset>
-                <UIcon name="i-heroicons-bell" class="w-5 h-5" />
-              </UChip>
-            </UButton>
-          </UTooltip>
-
-          <UDropdown :items="items">
-            <UButton icon="i-heroicons-plus" size="md" class="ml-1.5 rounded-full" />
-          </UDropdown>
-        </template>
+       
       </UDashboardNavbar>
 
       <UDashboardPanelContent>
