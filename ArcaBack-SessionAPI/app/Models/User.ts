a33ea1889e5192542bc9d4c1dbb03dea7db6eb6e
adrienmorin1 <adridu71170@gmail.com>
2024-04-19 09:46:01 +0200

    feat/fetchUsers

--- ArcaBack-SessionAPI/app/Models/User.ts
+++ ArcaBack-SessionAPI/app/Models/User.ts
@@ -27,6 +27,9 @@ export default class User extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 
+  @column()
+  public role= "user"
+
   @beforeSave()
   public static async hashPassword (user: User) {
     if (user.$dirty.password) {
