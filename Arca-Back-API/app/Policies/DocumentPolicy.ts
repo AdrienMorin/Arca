75aa05241912c9ca4f491eccda847605f25e50c0
Torvikzer <lesaint.victor@outlook.fr>
2024-04-25 11:33:41 +0200

    documentPolicy modified

--- Arca-Back-API/app/Policies/DocumentPolicy.ts
+++ Arca-Back-API/app/Policies/DocumentPolicy.ts
@@ -3,18 +3,18 @@ import User from 'App/Models/User'
 
 export default class DocumentPolicy extends BasePolicy {
     public async viewList(user: User) {
-		return user.role === 'admin'
+		return (user.role === 'admin'||user.role === 'user'||user.role === 'superUser')
 	} 
 	public async view(user: User) {
-		return user.role === 'admin'
+		return (user.role === 'admin'||user.role === 'user'||user.role === 'superUser')
 	}
 	public async create(user: User) {
-		return user.role === 'admin'
+		return (user.role === 'admin'||user.role === 'user'||user.role === 'superUser')
 	}
 	public async update(user: User) {
-		return user.role === 'admin'
+		return (user.role === 'admin'||user.role === 'user'||user.role === 'superUser')
 	}
 	public async delete(user: User) {
-		return user.role === 'admin'
+		return (user.role === 'admin'||user.role === 'user'||user.role === 'superUser')
 	}
 }
