4af46d3652e74463622973dabdf07597cc666a2c
Gr1Gny <97124361+Gr1Gny@users.noreply.github.com>
2024-04-26 15:43:26 +0200

    Update Archiver.vue

--- Arca-Front/pages/Archiver.vue
+++ Arca-Front/pages/Archiver.vue
@@ -1,6 +1,45 @@
-<script setup>
-</script>
-
 <template>
-<h1>page archiverrrrrrrrrr</h1>
+  <div class="relative h-[calc(100vh-80px)]">
+    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 drop-document-container max-w-[50%] max-h-[60%] mx-auto text-center border-2 border-[#027BCE] rounded-[10px] p-[7%]">
+      <h1 class="text-[#000] mb-[16px]">Déposer un document</h1>
+      <div class="drop-area py-[20px]"
+           @dragover.prevent="handleDragOver"
+           @drop.prevent="handleFileDrop">
+        <p class="font-bold m-0">Faites glisser votre document ici</p>
+        <span class="block my-[10px]">ou</span>
+        <input type="file" id="fileInput" ref="fileInput" @change="handleFileSelect" style="display: none;" />
+        <button type="button"
+                class="browse-button bg-[#007BFF] text-white border-none rounded-[20px] px-[20px] py-[10px] text-[16px] cursor-pointer outline-none hover:bg-[#0056b3]"
+                @click="triggerFileInput">Parcourir vos fichiers</button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const fileInput = ref<HTMLInputElement | null>(null);
+
+function handleDragOver(event: DragEvent) {
+  event.preventDefault();
+}
+
+function handleFileDrop(event: DragEvent) {
+  if (event.dataTransfer?.files) {
+    const file = event.dataTransfer.files[0];
+    console.log("File dropped:", file.name);
+  }
+}
+
+function handleFileSelect(event: Event) {
+  const files = (event.target as HTMLInputElement).files;
+  if (files) {
+    console.log("File selected:", files[0].name);
+  }
+}
+
+function triggerFileInput() {
+  fileInput.value?.click();
+}
+</script>
