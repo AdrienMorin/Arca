f02ce29cf6a5ecc76ca9d61fb4b8a625a92b059b
Laziza Bellahbib <aziza.bellahbib@insa-lyon.fr>
2024-04-27 23:56:57 +0200

    middlewareAuth-Functioning

--- Arca-Front/middleware/auth.ts
+++ Arca-Front/middleware/auth.ts
@@ -1,10 +1,15 @@
-export default defineNuxtRouteMiddleware((to, from)=> {
-    console.log(to);
-    console.log(from);
-
-    const isLoggedIn = false;
-    if(isLoggedIn){
-        return navigateTo(to.fullPath);
+import axios from "~/node_modules/axios/index";
+export default defineNuxtRouteMiddleware(async(to, from)=> {
+    try {
+        const response = await axios.get(this.$config.public.API_URL);
+        if (response.data.isAuthenticated) {
+            return navigateTo(to.fullPath);
+        }
+        
+        if (to.path !== '/login') {
+            return navigateTo('/login');
+        }
+    } catch (error) {
+        console.error('Error checking session cookie:', error);
     }
-    return navigateTo('/rechercher');
 });
