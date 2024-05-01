07ec0f15df01eae8479189f2bed5696d01820b12
adrienmorin1 <adridu71170@gmail.com>
2024-05-01 17:27:45 +0200

    feat/admin

--- Arca-Front/middleware/authAdmin.ts
+++ Arca-Front/middleware/authAdmin.ts
@@ -1,20 +1,18 @@
 import axios from '~/node_modules/axios/index'
 
 export default defineNuxtRouteMiddleware(async(to, from)=> {
-  console.log('auth middleware')
   try {
     let tokenCookie = useCookie('token')
     const token = tokenCookie.value
 
-    const response = await axios.get('https://127.0.0.1:3333/api', {
+    const response = await axios.get('https://127.0.0.1:3333/api/auth/isLoggedInAsAdmin', {
       headers: {
         Authorization: `Bearer ${token}`
       }
     })
 
     console.log('response:', response.data)
-    if (response.data !== 'Vous êtes connecté') {
-      console.log('Vous êtes pas connecté')
+    if (response.data.message !== "Vous êtes connecté en tant qu'administrateur") {
       return navigateTo('/login')
     }
 
