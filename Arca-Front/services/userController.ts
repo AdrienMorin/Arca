182c802cb90498815a1a08258d8f5b14a4a26c2c
Laziza Bellahbib <aziza.bellahbib@insa-lyon.fr>
2024-05-01 17:23:00 +0200

    fixPathBack

--- Arca-Front/services/userController.ts
+++ Arca-Front/services/userController.ts
@@ -36,7 +36,7 @@ class UserController {
   }
 
   public async getUser(token: string):Promise<any>{
-    const response = await axios.get(`${baseUrl}/getUser`, {
+    const response = await axios.get(`${baseUrl}/user/getUser`, {
       headers: {
         Authorization: `Bearer ${token}`
       }
@@ -92,7 +92,7 @@ class UserController {
     oldPassword: string,
     newPassword: string
   ): Promise<any> {
-    const response = await axios.post(`${baseUrl}/changePassword`, 
+    const response = await axios.post(`${baseUrl}/user/changePassword`, 
     {oldPassword, newPassword},
     {
       headers: {
