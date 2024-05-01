93ca46c80d09fee3c92a0a0c4aea262f6ed5555a
Torvikzer <lesaint.victor@outlook.fr>
2024-05-01 11:03:17 +0200

    la pipeline brrrrr

--- Arca-Back-API/app/Validators/AI/CreateAiDocumentValidator.ts
+++ Arca-Back-API/app/Validators/AI/CreateAiDocumentValidator.ts
@@ -5,18 +5,10 @@ export default class CreateAiDocumentValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    _id: schema.string({}, [
-      rules.minLength(3),
-      rules.maxLength(255)
-    ]),
-    s3_id: schema.string({}, [
-      rules.minLength(3),
-      rules.maxLength(255)
-    ]),
     file: schema.file({
       size: '10mb',
       extnames: ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx','jpg','png','jpeg','webp','mp3','wav','aac']
-    }),
+    })
   })
 
   public messages: CustomMessages = {}
