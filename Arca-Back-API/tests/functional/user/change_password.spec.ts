ad2b2dd1df6725f746799a3c853f34d7e9fb87b9
Torvikzer <lesaint.victor@outlook.fr>
2024-05-02 17:29:17 +0200

    modification tests password

--- Arca-Back-API/tests/functional/user/change_password.spec.ts
+++ Arca-Back-API/tests/functional/user/change_password.spec.ts
@@ -2,19 +2,15 @@ import { test } from "@japa/runner";
 import User from "App/Models/User";
 
 test("changePassword Super user ", async ({ client }) => {
-  const user = await User.create({
-    firstname: "pass",
-    lastname: "word",
-    email: "password@gmail.com",
-    password: "password1",
-    role: "superuser",
-  });
+  const user = await User.find(1);
+  const oldPassword = "superuser";
+  const newPassword = "password2";
 
   const response = await client
-    .post("/api/changePassword")
+    .post("/api/user/changePassword")
     .json({
-      oldPassword: "password1",
-      newPassword: "password2",
+      oldPassword: oldPassword,
+      newPassword: newPassword,
     })
     .loginAs(user!);
 
@@ -23,34 +19,29 @@ test("changePassword Super user ", async ({ client }) => {
   if (response.status() === 200) {
     const response = await client.post("/api/auth/login").json({
       email: user!.email,
-      password: "password2",
+      password: newPassword,
     });
     response.assertStatus(200);
 
     const response2 = await client.post("/api/auth/login").json({
       email: user!.email,
-      password: "password1",
+      password: oldPassword,
     });
     response2.assertStatus(401);
 
-    await user.delete();
   }
 });
 
 test("changePassword admin ", async ({ client }) => {
-  const user = await User.create({
-    firstname: "pass",
-    lastname: "word",
-    email: "password@gmail.com",
-    password: "password1",
-    role: "admin",
-  });
+  const user = await User.find(2);
+  const oldPassword = "raffolgo";
+  const newPassword = "password2";
 
   const response = await client
-    .post("/api/changePassword")
+    .post("/api/user/changePassword")
     .json({
-      oldPassword: "password1",
-      newPassword: "password2",
+      oldPassword: oldPassword,
+      newPassword: newPassword,
     })
     .loginAs(user!);
 
@@ -59,34 +50,29 @@ test("changePassword admin ", async ({ client }) => {
   if (response.status() === 200) {
     const response = await client.post("/api/auth/login").json({
       email: user!.email,
-      password: "password2",
+      password: newPassword,
     });
     response.assertStatus(200);
 
     const response2 = await client.post("/api/auth/login").json({
       email: user!.email,
-      password: "password1",
+      password: oldPassword,
     });
     response2.assertStatus(401);
 
-    await user.delete();
   }
 });
 
 test("changePassword user ", async ({ client }) => {
-  const user = await User.create({
-    firstname: "pass",
-    lastname: "word",
-    email: "password@gmail.com",
-    password: "password1",
-    role: "user",
-  });
+  const user = await User.find(3);
+  const oldPassword = "adLaurent";
+  const newPassword = "password2";
 
   const response = await client
-    .post("/api/changePassword")
+    .post("/api/user/changePassword")
     .json({
-      oldPassword: "password1",
-      newPassword: "password2",
+      oldPassword: oldPassword,
+      newPassword: newPassword,
     })
     .loginAs(user!);
 
@@ -95,47 +81,17 @@ test("changePassword user ", async ({ client }) => {
   if (response.status() === 200) {
     const response = await client.post("/api/auth/login").json({
       email: user!.email,
-      password: "password2",
+      password: newPassword,
     });
     response.assertStatus(200);
 
     const response2 = await client.post("/api/auth/login").json({
       email: user!.email,
-      password: "password1",
+      password: oldPassword,
     });
     response2.assertStatus(401);
 
-    await user.delete();
   }
 });
 
-test("changePassword samePassword user ", async ({ client }) => {
-  const user = await User.create({
-    firstname: "pass",
-    lastname: "word",
-    email: "password@gmail.com",
-    password: "password1",
-    role: "superuser",
-  });
 
-  const response = await client
-    .post("/api/changePassword")
-    .json({
-      oldPassword: "password1",
-      newPassword: "password1",
-    })
-    .loginAs(user!);
-
-  response.assertStatus(200); //devrait pas marcher ?
-
-  if (response.status() === 200) {
-    const response2 = await client.post("/api/auth/login").json({
-      email: user!.email,
-      password: "password1",
-    });
-    response2.assertStatus(200);
-    if (response2.status() === 200) {
-      await user.delete();
-    }
-  }
-});
