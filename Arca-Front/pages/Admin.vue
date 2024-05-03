286fd9f9a5b6ff1f3b6c902508bdcbf0c20bbfb8
adrienmorin1 <adridu71170@gmail.com>
2024-05-03 17:32:06 +0200

    admin/personnes

--- Arca-Front/pages/Admin.vue
+++ Arca-Front/pages/Admin.vue
@@ -13,7 +13,7 @@ export default {
   components: {
     UsersComponent,
     CategoriesComponent,
-    VillesComponent
+    PersonnesComponent
   },
   async mounted() {
     const tokenCookie = useCookie('token')
