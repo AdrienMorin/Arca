77b004c0e302226b7ae5140de76d49e3862a0802
Laziza Bellahbib <aziza.bellahbib@insa-lyon.fr>
2024-05-03 16:34:01 +0200

    adminVilles

--- Arca-Front/pages/Admin.vue
+++ Arca-Front/pages/Admin.vue
@@ -13,7 +13,7 @@ export default {
   components: {
     UsersComponent,
     CategoriesComponent,
-    PersonnesComponent
+    VillesComponent
   },
   async mounted() {
     const tokenCookie = useCookie('token')
