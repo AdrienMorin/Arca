a6be28e4c394078d7c9abcba417956f4d15c6efc
Torvikzer <lesaint.victor@outlook.fr>
2024-05-03 16:44:53 +0200

    added the API call to the backend for AI upload

--- Arca-Front/pages/Archiver.vue
+++ Arca-Front/pages/Archiver.vue
@@ -1,18 +1,14 @@
 <template>
 
-  <div v-if="showModal" class="fixed z-50 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" @click="showModal = false">
-      </div>
-      <!-- Modal content -->
-      <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-          <!-- Modal content goes here -->
-          <p class="text-center">Votre document est en train d'être téléchargé, veuillez attendre un peu</p>
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="h-screen overflow-hidden">
+
+        <Popup v-if="mainshow"
+          ref="popupConfirmation"
+          :title="'Annuler Modifications'"
+          :description1="'Etes-vous sûr de vouloir annuler l`ajout du document suivant'"
+          :titreDoc="titreDoc"
+          :annuler="true" />
+
 <div class="flex justify-center items-center h-screen mt-[-5%]">
   <div class="drop-document-container w-[45%] max-h-[50%]  text-center border-2 border-[#027BCE] rounded-[10px] p-[8%]" v-if="!uploaded">
     <div >
@@ -31,6 +27,7 @@
     </div>
   </div>
 
+
   <div class="drop-document-container w-[45%] max-h-[50%] flex flex-col justify-center items-center border-2 border-[#027BCE] rounded-[10px]  p-[2%]" v-else>
   <div class="flex-col justify-center items-center  w-2/3 h-2/3 p-[5%]">
     <div class="flex justify-center items-center">
@@ -39,13 +36,13 @@
     <p class="text-[#000] mb-[12px] text-sm text-center" >{{ fileName }}</p>
     <h1 class="text-[#000] mb-[16px] text-center">Document déposé</h1>
   </div>
-
+</div>
   <div class="flex w-full h-1/2 text-center justify-between p-[3%]">
     <div class="flex justify-center items-center relative w-1/3">
       <button type="button" class="browse-button bg-[#007BFF] text-white border-none rounded-[20px] px-[20px]  w-full py-[10px] text-[20px] cursor-pointer outline-none hover:bg-[#0056b3]" @click="fileTransfer()">Saisie Manuelle</button>
     </div>
     <div class="flex justify-center items-center  relative w-1/3">
-      <button type="button" class="browse-button bg-[#007BFF] text-white -none rounded-[20px] px-[15px]  w-full py-[10px] text-[20px] cursor-pointer outline-none hover:bg-[#0056b3]" @click="aiFileTransfer">Analyse Automatique</button>
+      <button type="button" class="browse-button bg-[#007BFF] text-white -none rounded-[20px] px-[15px]  w-full py-[10px] text-[20px] cursor-pointer outline-none hover:bg-[#0056b3]" @click="aiFileTransfer()">Analyse Automatique</button>
     </div>
   </div>
 </div>
@@ -54,6 +51,7 @@
 
 
 </div>
+
 
   
 
@@ -67,7 +65,7 @@ import { DocumentCheckIcon } from '@heroicons/vue/24/outline';
 import axios from 'axios';
 import { useFileStore } from '~/fileTransfer';
 import UserController from '~/services/userController.ts';
-import ConfpopupMdp from '~/components/users/popup_ConfChangeMdp.vue';
+import Popup from '~/components/users/popup.vue';
 
 definePageMeta({
   middleware:'auth',
@@ -75,7 +73,7 @@ definePageMeta({
 export default {
   components: {
     DocumentCheckIcon,
-    ConfpopupMdp,
+    Popup,
   },
 
   data() {
@@ -123,26 +121,21 @@ export default {
 
       async aiFileTransfer(){
         console.log('File transfer initiated...');
-        const tokenCookie = useCookie('token');
+        const tokenCookie = useCookie('token')
         const token= tokenCookie.value;
-        try {
-          this.openModal()
-          const response = await UserController.getInstance().uploadAiDocument(token,this.file);
-          
-          if (response.status === 200) {
-            this.$snackbar.add({ type:'success',text: 'Le téléchargement a réussi.' });  
-          } else {
-            this.$snackbar.add({ type:'fail',text: "Le téléchargement n'a pas réussi." });  
-          }
-        } catch (error) {
-          this.$snackbar.add({ type:'fail',text: "Le téléchargement n'a pas réussi." });  
+        const response = await UserController.getInstance().uploadAiDocument(token,this.file);
+
+        if (response.status === 200) {
+          console.log('Document uploaded successfully');
+          window.alert('Le téléchargement du document a réussi');
+        }else{
+          console.log('Document upload failed');
+          window.alert('Le téléchargement du document a échoué, veuillez réessayer dans quelques minutes');
         }
         
 
         
       },
-
-
 
       fileTransfer(){
         console.log('File transfer initiated...');
--- Arca-Front/services/userController.ts
+++ Arca-Front/services/userController.ts
@@ -187,23 +187,22 @@ class UserController {
     return response;
   }
 
-
-  public async getDocument(
+  public async uploadAiDocument(
     token: string,
-    s3_name: string,
-
+    file: File,
   ): Promise<any> {
-    const response = await axios.post(`${baseUrl}/basic/get`,
-      {s3_name},
+    const response = await axios.post(`${baseUrl}/ai/upload`,
+      {file},
       {
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "multipart/form-data",
+
         }
       });
+    console.log(response);
     return response;
   }
-
 
   public async fetchCategories(token: string):Promise<any>{
     const response = await axios.get(`${baseUrl}/category/fetchCategories`, {
