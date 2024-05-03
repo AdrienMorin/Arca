a01a27acc148ea32f6f02873045260c59618f3df
gagathiou <autieragathe@gmail.com>
2024-05-03 17:37:05 +0200

    json to front working

--- Arca-Front/services/userController.ts
+++ Arca-Front/services/userController.ts
@@ -169,75 +169,19 @@ class UserController {
   }
 
 
-  public async getSearchResults(
-    token: string,
-    query: string,
-
-  ): Promise<any> {
-    const response = await axios.post(`${baseUrl}/search`,
-      {query},
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "multipart/form-data",
-
-        }
-      });
-    console.log(response);
-    return response;
-  }
-
-  public async uploadAiDocument(
-    token: string,
-    file: File,
-  ): Promise<any> {
-    const response = await axios.post(`${baseUrl}/ai/upload`,
-      {file},
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "multipart/form-data",
-
-        }
-      });
-    console.log(response);
-    return response;
-  }
-
   public async getDocument(
     token: string,
     s3_name: string,
 
   ): Promise<any> {
-    console.log("++++++++++++++++++++++++++++");
     const response = await axios.post(`${baseUrl}/basic/get`,
       {s3_name},
       {
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "multipart/form-data",
-
         }
       });
-    console.log("nom"+s3_name);
-    return response;
-  }
-
-  public async uploadAiDocument(
-    token: string,
-    file: File,
-
-  ): Promise<any> {
-    const response = await axios.post(`${baseUrl}/ai/upload`,
-      {file},
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "multipart/form-data",
-
-        }
-      });
-    console.log(response);
     return response;
   }
 
