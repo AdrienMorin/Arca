3c3e1feaa1508595cd7163f1bcc857b2af2b77d6
gagathiou <autieragathe@gmail.com>
2024-05-03 15:50:22 +0200

    pipeline working from s3 to front with improvements

--- Arca-Front/services/userController.ts
+++ Arca-Front/services/userController.ts
@@ -203,7 +203,24 @@ class UserController {
     console.log(response);
     return response;
   }
+  public async getDocument(
+    token: string,
+    s3_name: string,
 
+  ): Promise<any> {
+    console.log("++++++++++++++++++++++++++++");
+    const response = await axios.post(`${baseUrl}/basic/get`,
+      {s3_name},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+
+        }
+      });
+    console.log("nom"+s3_name);
+    return response;
+  }
   public async fetchCategories(token: string):Promise<any>{
     const response = await axios.get(`${baseUrl}/category/fetchCategories`, {
       headers: {
