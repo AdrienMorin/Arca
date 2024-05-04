eed2b1daaf73e76789f69cb469da68bc6198290e
Torvikzer <lesaint.victor@outlook.fr>
2024-05-04 23:35:12 +0200

    added a route to get doc by id in review db

--- Arca-Back-API/app/Controllers/Http/GetPipelinesController.ts
+++ Arca-Back-API/app/Controllers/Http/GetPipelinesController.ts
@@ -48,6 +48,51 @@ export default class GetPipelinesController {
         return response.status(200).json(result)
         
     }
+    
+    public async getDocReview({ auth,bouncer,response,request}:HttpContextContract){   
+        await auth.use('api').authenticate()
+        await bouncer.with('AiPolicy').authorize('create')
+
+       await client.connect();
+       await client.db("admin").command({ ping: 1 });
+
+        const payload = await request.validate(GetPipelineValidator)    
+        const s3_name = payload.s3_name;
+
+        const tempFolderPath = '../Arca-Front/temp'; 
+        const tempFilePath = `${tempFolderPath}/${s3_name}`;
+
+        fs.readdirSync(tempFolderPath).forEach((file) => {
+            const filePath = `${tempFolderPath}/${file}`;
+            fs.unlinkSync(filePath);
+        });
+        const contents = await Drive.get(s3_name);
+        fs.writeFileSync(tempFilePath, contents);
+
+        console.log("s3 done")
+
+        const id = s3_name.split('.')[0];
+        const result = await client.db("reviewDB").collection("review").findOne({ _id: id });
+
+        interface Person {
+            id: string;
+            name: string;
+        }
+
+        const peopleList: Person[] = [];
+
+        for (const personId of result.people) {
+            const fetchedPerson = await Person.findOrFail(personId);
+            peopleList.push({ id: personId, name: fetchedPerson.displayname });
+        }
+        result.people = peopleList
+
+        console.log(result);
+        console.log("mongo db done")
+
+        return response.status(200).json(result)
+        
+    }
 
     public async deleteDocumentArca({ request, response }: HttpContextContract) {
         const filename = request.input('filename');
