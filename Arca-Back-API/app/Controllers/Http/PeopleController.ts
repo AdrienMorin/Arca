f1c097d493ce4204579714ed3a32dc39af259557
Torvikzer <lesaint.victor@outlook.fr>
2024-05-04 23:35:12 +0200

    added a route to get doc by id in review db

--- Arca-Back-API/app/Controllers/Http/PeopleController.ts
+++ Arca-Back-API/app/Controllers/Http/PeopleController.ts
@@ -50,7 +50,7 @@ export default class PeopleController {
         await bouncer.with('PersonPolicy').authorize('viewList')
         const allDocuments = await Database
             .from('people')
-            .join('locations', 'people.location', '=', 'locations.id')
+            .leftJoin('locations', 'people.location', '=', 'locations.id')
             .select('people.*')
             .select('locations.displayname as location')
 
