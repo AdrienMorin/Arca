12e1feb7d21ec99bc15f877a492abfd8ea53c820
Torvikzer <lesaint.victor@outlook.fr>
2024-05-04 22:38:24 +0200

    fixed bugs for front call to python api

--- Arca-Front/pages/Archiver.vue
+++ Arca-Front/pages/Archiver.vue
@@ -1,14 +1,4 @@
 <template>
-
-<div class="h-screen overflow-hidden">
-
-        <Popup v-if="mainshow"
-          ref="popupConfirmation"
-          :title="'Annuler Modifications'"
-          :description1="'Etes-vous sûr de vouloir annuler l`ajout du document suivant'"
-          :titreDoc="titreDoc"
-          :annuler="true" />
-
 <div class="flex justify-center items-center h-screen mt-[-5%]">
   <div class="drop-document-container w-[45%] max-h-[50%]  text-center border-2 border-[#027BCE] rounded-[10px] p-[8%]" v-if="!uploaded">
     <div >
@@ -27,7 +17,6 @@
     </div>
   </div>
 
-
   <div class="drop-document-container w-[45%] max-h-[50%] flex flex-col justify-center items-center border-2 border-[#027BCE] rounded-[10px]  p-[2%]" v-else>
   <div class="flex-col justify-center items-center  w-2/3 h-2/3 p-[5%]">
     <div class="flex justify-center items-center">
@@ -36,13 +25,13 @@
     <p class="text-[#000] mb-[12px] text-sm text-center" >{{ fileName }}</p>
     <h1 class="text-[#000] mb-[16px] text-center">Document déposé</h1>
   </div>
-</div>
+
   <div class="flex w-full h-1/2 text-center justify-between p-[3%]">
     <div class="flex justify-center items-center relative w-1/3">
       <button type="button" class="browse-button bg-[#007BFF] text-white border-none rounded-[20px] px-[20px]  w-full py-[10px] text-[20px] cursor-pointer outline-none hover:bg-[#0056b3]" @click="fileTransfer()">Saisie Manuelle</button>
     </div>
     <div class="flex justify-center items-center  relative w-1/3">
-      <button type="button" class="browse-button bg-[#007BFF] text-white -none rounded-[20px] px-[15px]  w-full py-[10px] text-[20px] cursor-pointer outline-none hover:bg-[#0056b3]" @click="aiFileTransfer()">Analyse Automatique</button>
+      <button type="button" class="browse-button bg-[#007BFF] text-white -none rounded-[20px] px-[15px]  w-full py-[10px] text-[20px] cursor-pointer outline-none hover:bg-[#0056b3]" @click="aiFileTransfer">Analyse Automatique</button>
     </div>
   </div>
 </div>
@@ -51,7 +40,6 @@
 
 
 </div>
-
 
   
 
@@ -65,7 +53,7 @@ import { DocumentCheckIcon } from '@heroicons/vue/24/outline';
 import axios from 'axios';
 import { useFileStore } from '~/fileTransfer';
 import UserController from '~/services/userController.ts';
-import Popup from '~/components/users/popup.vue';
+import ConfpopupMdp from '~/components/users/popup_ConfChangeMdp.vue';
 
 definePageMeta({
   middleware:'auth',
@@ -73,7 +61,7 @@ definePageMeta({
 export default {
   components: {
     DocumentCheckIcon,
-    Popup,
+    ConfpopupMdp,
   },
 
   data() {
@@ -114,21 +102,25 @@ export default {
 
       async aiFileTransfer(){
         console.log('File transfer initiated...');
-        const tokenCookie = useCookie('token')
+        const tokenCookie = useCookie('token');
         const token= tokenCookie.value;
-        const response = await UserController.getInstance().uploadAiDocument(token,this.file);
-
-        if (response.status === 200) {
-          console.log('Document uploaded successfully');
-          window.alert('Le téléchargement du document a réussi');
-        }else{
-          console.log('Document upload failed');
-          window.alert('Le téléchargement du document a échoué, veuillez réessayer dans quelques minutes');
+        try {
+          const response = await UserController.getInstance().uploadAiDocument(token,this.file);
+          
+          if (response.status === 200) {
+            window.alert('Le téléchargement a réussi.');
+          } else {
+            window.alert('Le téléchargement a échoué. Veuillez réessayer.');
+          }
+        } catch (error) {
+          window.alert('Une erreur a eu lieu lors du téléchargement: ' + error.message);
         }
         
 
         
       },
+
+
 
       fileTransfer(){
         console.log('File transfer initiated...');
