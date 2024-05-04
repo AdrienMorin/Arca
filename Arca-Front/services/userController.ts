b7ab0ed824bf49ac4887ac3fb494b2e53901b4c3
Torvikzer <lesaint.victor@outlook.fr>
2024-05-04 22:38:24 +0200

    fixed bugs for front call to python api

--- Arca-Front/services/userController.ts
+++ Arca-Front/services/userController.ts
@@ -184,6 +184,7 @@ class UserController {
     console.log(response);
     return response;
   }
+
   public async getDocument(
     token: string,
     s3_name: string,
@@ -203,11 +204,12 @@ class UserController {
     return response;
   }
 
- public async uploadAiDocument(
+  public async uploadAiDocument(
     token: string,
     file: File,
+
   ): Promise<any> {
-    const response = await axios.post(`${baseUrl}/basic/upload`,
+    const response = await axios.post(`${baseUrl}/ai/upload`,
       {file},
       {
         headers: {
