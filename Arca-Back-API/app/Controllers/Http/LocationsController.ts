32c3568823427c230c385bebf923ff1111984740
Torvikzer <lesaint.victor@outlook.fr>
2024-05-06 11:31:47 +0200

    snackbar to location admin view

--- Arca-Back-API/app/Controllers/Http/LocationsController.ts
+++ Arca-Back-API/app/Controllers/Http/LocationsController.ts
@@ -8,16 +8,24 @@ export default class LocationsController {
         await auth.use('api').authenticate()
         await bouncer.with('LocationPolicy').authorize('create')
         const payload = await request.validate(CreateLocationValidator)
+        try{
+            await Location.create({
+                cityname: payload.cityname,
+                regionname: payload.regionname,
+                zipcode: payload.zipcode,
+                country: payload.country,
+                displayname: payload.cityname+ ', ' + payload.regionname + ', ' + payload.country
+            })
+        }catch (error){
+            if(error.code === '23505'){
+                return response.status(400).json({message: 'Lieu déjà existant', error: error})
+            }else{
+                return response.status(500).json({message: 'Erreur lors de la création du lieu', error: error})
+            }
+        }
 
-        await Location.create({
-            cityname: payload.cityname,
-            regionname: payload.regionname,
-            zipcode: payload.zipcode,
-            country: payload.country,
-            displayname: payload.cityname+ ', ' + payload.regionname + ', ' + payload.country
-        })
         return response.status(200).json({message: 'Lieu créé avec succès'})
-    }false
+    }
 
 
     public async fetchLocations({auth, bouncer, response}: HttpContextContract){
