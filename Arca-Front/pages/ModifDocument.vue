27728cfa9b65a52673d961e1bbdac892b7b4f72d
gagathiou <autieragathe@gmail.com>
2024-05-06 15:32:55 +0200

    first try

--- Arca-Front/pages/ModifDocument.vue
+++ Arca-Front/pages/ModifDocument.vue
@@ -1,4 +1,3 @@
-
 <script lang="ts">
   
   import doctype from '~/components/users/Document-type-dropdown.vue';
@@ -30,7 +29,7 @@
     },
      data() {
       return {
-        nom: '', // Initialize with an empty string
+        nom: '041zxozsr27ilvqdv22p.jpg',
         File: '',
         titre: '',
         description: '',
@@ -51,21 +50,19 @@
         mongoDB: '',
       };
     },
+
     async created() {
-    this.getDocument();
-    //console.log('Fetching Personnes...');
-    const fetchedPersonnes = await this.fetchPersonnes();
-    this.personnes = fetchedPersonnes.data;
-   // console.log('Personnes fetched:', this.personnes);
-    const fetchedCities = await this.fetchCities();
-    this.cities = fetchedCities.data;
-    this.cities = this.cities.map(city => city.displayname);
-    console.log('fetch categories');
-    const fetchedCategories = await this.fetchCategories();
-    this.categories = fetchedCategories.data;
+      this.getDocument();
+      const fetchedPersonnes = await this.fetchPersonnes();
+      this.personnes = fetchedPersonnes.data;
+      const fetchedCities = await this.fetchCities();
+      this.cities = fetchedCities.data;
+      this.cities = this.cities.map(city => city.displayname);
+      console.log('fetch categories');
+      const fetchedCategories = await this.fetchCategories();
+      this.categories = fetchedCategories.data;
     },
   
-      
     methods: {
       async fetchCategories() {
       const tokenCookie = useCookie('token');
@@ -98,55 +95,65 @@
         this.$refs.personneAajoute.search = '';
       }
     },
-
     refreshPage() {
       location.reload();
     }, 
     flipAnnuler() {
     this.$refs.popupAnnul.mainshow = !this.$refs.popupAnnul.mainshow;
      }, 
-    flipAjouter() {
-      this.$refs.popupAjout.mainshow = !this.$refs.popupAjout.mainshow;
+    flipModifier() {
+      this.$refs.popupModif.mainshow = !this.$refs.popupModif.mainshow;
     },
     flipEnregistrer() {
       this.$refs.popupEnregistrer.mainshow = !this.$refs.popupEnregistrer.mainshow;
     },
-    
-    async uploadDocument(db) {
-      console.log(this.getSelectedCategorie());
-        this.getListCitier();
-        this.getListPersonne();
-        this.citiesListe='';
-        this.personneListe='';
-        for (let i = 0; i < this.selectedCities.length; i++) {
-          this.citiesListe+=this.selectedCities[i];
-          if(i!=this.selectedCities.length-1){
-            this.citiesListe+=';';
-          }
-        }
-        for (let i = 0; i < this.selectedPersonne.length; i++) {
-          this.personneListe+=this.selectedPersonne[i];
-          if(i!=this.selectedPersonne.length-1){
-            this.personneListe+=';';
-          }
-        }
-        this.description=this.$refs.description.description;
-        this.retrancription=this.$refs.retranscription.description;
-        this.mongoDB = db;
-        const tokenCookie = useCookie('token');
-        const token= tokenCookie.value;
+    flipSupprimer() {
+      this.$refs.popupSupprimer.mainshow = !this.$refs.popupSupprimer.mainshow;
+    },
 
- 
-        console.log(this.File);
+    getDocument() {
+      console.log('Checking document...');
+      const fileStore = useFileStore();
+      const test=fileStore.getFile;
+      this.File=test.content;
+      this.titreDoc=test.name;
+    },
+
+    handleSelectedRange(range) {
+      this.date.start= range.start;
+      this.date.start=new Date(range.start).toISOString();
+      this.date.end= new Date(range.end).toISOString();
+    },
+
+    async uploadDocument(db) {
+      this.getListCitier();
+      this.getListPersonne();
+      this.citiesListe='';
+      this.personneListe='';
+      for (let i = 0; i < this.selectedCities.length; i++) {
+        this.citiesListe+=this.selectedCities[i];
+        if(i!=this.selectedCities.length-1){
+          this.citiesListe+=';';
+        }
+      }
+      for (let i = 0; i < this.selectedPersonne.length; i++) {
+        this.personneListe+=this.selectedPersonne[i];
+        if(i!=this.selectedPersonne.length-1){
+          this.personneListe+=';';
+        }
+      }
+      this.description=this.$refs.description.description;
+      this.retrancription=this.$refs.retranscription.description;
+      this.mongoDB = db;
+      const tokenCookie = useCookie('token');
+      const token= tokenCookie.value;
+
       const response = await UserController.getInstance().uploadDocument(token,
       this.File,this.titre,this.description,this.retrancription,this.date,this.citiesListe,this.personneListe,this.mongoDB);
-      
-      
-      // redirect to homepage if user is authenticated
+            
       if (response.status === 200) {
         console.log('Vous êtes connecté')
         this.flipAjouter();
-        //this.$router.push('/rechercher');
       }
     },
 
@@ -174,27 +181,67 @@
       const token= tokenCookie.value;
 
       const response = await UserController.getInstance().updateDocument(token,
-      this.File,this.titre,this.description,this.retrancription,this.date,this.citiesListe,this.personneListe,this.mongoDB, this.id);
+      this.File,this.titre,this.description,this.retrancription,this.date,this.citiesListe,this.personneListe,this.mongoDB, this.nom);
       
       if (response.status === 200) {
         console.log('Vous êtes connecté')
-        this.flipAjouter();
+        this.flipModifier();
       }
     },
 
-    getDocument() {
-      console.log('Checking document...');
-      const fileStore = useFileStore();
-      const test=fileStore.getFile;
-      this.File=test.content;
-      this.titreDoc=test.name;
+    async transferDocumentById() {
+      this.getListCitier();
+      this.getListPersonne();
+      this.citiesListe='';
+      this.personneListe='';
+      for (let i = 0; i < this.selectedCities.length; i++) {
+        this.citiesListe+=this.selectedCities[i];
+        if(i!=this.selectedCities.length-1){
+          this.citiesListe+=';';
+        }
+      }
+      for (let i = 0; i < this.selectedPersonne.length; i++) {
+        this.personneListe+=this.selectedPersonne[i];
+        if(i!=this.selectedPersonne.length-1){
+          this.personneListe+=';';
+        }
+      }
+      this.description=this.$refs.description.description;
+      this.retrancription=this.$refs.retranscription.description;
+      this.mongoDB = db;
+      const tokenCookie = useCookie('token');
+      const token= tokenCookie.value;
+
+      const response = await UserController.getInstance().transferDocumentById(token,this.nom);
+      
+      if (response.status === 200) {
+        console.log('Vous êtes connecté')
+        this.flipModifier();
+      }
     },
 
-    handleSelectedRange(range) {
-      this.date.start= range.start;
-      this.date.start=new Date(range.start).toISOString();
-      this.date.end= new Date(range.end).toISOString();
-    }
+    async deleteDocument() {
+      const tokenCookie = useCookie('token');
+      const token= tokenCookie.value;
+      const response = await UserController.getInstance().deleteDocuments(token,this.nom,this.mongoDB);
+      
+      if (response.status === 200) {
+        console.log('Vous êtes connecté')
+        this.flipSupprimer();
+      }
+    },
+
+    async downloadDocument() {
+      const tokenCookie = useCookie('token');
+      const token= tokenCookie.value;
+      const response = await UserController.getInstance().deleteDocuments(token,this.nom,this.mongoDB);
+      
+      if (response.status === 200) {
+        console.log('Vous êtes connecté')
+        this.flipSupprimer();
+      }
+    },
+
   },
   };
 
@@ -209,72 +256,47 @@
   <div class="">
   <!--popup -->
   <div class="">
-  <!--popup -->
-        <Popup
-          ref="popupAnnul"
-          :title="'Annuler Modifications'"
-          :description1="'Etes-vous sûr de vouloir annuler l`ajout du document suivant'"
-          ref="popupAnnul"
-          :title="'Annuler Modifications'"
-          :description1="'Etes-vous sûr de vouloir annuler l`ajout du document suivant'"
-          :titreDoc="'Complainte du'"
-          :annuler="true" >
 
-          :annuler="true" >
+    <Popup
+      ref="popupAnnul"
+      :title="'Annuler Modifications'"
+      :description1="'Etes-vous sûr de vouloir annuler l`ajout du document suivant'"
+      :titreDoc="'Complainte du'"
+      :annuler="true" >
+    </Popup>
 
-        </Popup>
+    <Popup 
+      ref="popupModif"
+      :title="'Modifier Document'"
+      :description1="'Votre Document'"
+      :titreDoc="'Complainte du'"
+      :description2="'a bien été modifié dans la base de donnée.'"
+      :color="true" 
+      :annuler="false">
+    </Popup>
 
-        <Popup 
-          ref="popupAjout"
-          :title="'Ajouter Document'"
-        <Popup 
-          ref="popupAjout"
-          :title="'Ajouter Document'"
-          :description1="'Votre Document'"
-          :titreDoc="'Complainte du'"
-          :description2="'a bien été Ajouté à la base de donnée.'"
-          :color="true" 
-          :annuler="false">
-          :description2="'a bien été Ajouté à la base de donnée.'"
-          :color="true" 
-          :annuler="false">
-        </Popup>
+    <Popup 
+      ref="popupEnregistrer"
+      :title="'Ajouter Document'"
+      :description1="'Votre Document'"
+      :titreDoc="'Complainte du'"
+      :description2="'a bien été Enregistrée à la base de donnée et devrait être Vérifier par la suite.'"
+      :color="true" 
+      :annuler="false">
+    </Popup>
 
-        <Popup 
-          ref="popupEnregistrer"
-          :title="'Ajouter Document'"
-          :description1="'Votre Document'"
-        <Popup 
-          ref="popupEnregistrer"
-          :title="'Ajouter Document'"
-          :description1="'Votre Document'"
-          :titreDoc="'Complainte du'"
-          :description2="'a bien été Enregistrée à la base de donnée et devrait être Vérifier par la suite.'"
-          :color="true" 
-          :annuler="false">
-          :description2="'a bien été Enregistrée à la base de donnée et devrait être Vérifier par la suite.'"
-          :color="true" 
-          :annuler="false">
-        </Popup>
+    <Popup 
+      ref="popupSupprimer"
+      :title="'Supprimer Document'"
+      :description1="'Votre Document'"
+      :titreDoc="'Complainte du'"
+      :description2="'a bien été supprimé de la base de donnée.'"
+      :color="true" 
+      :annuler="false">
+    </Popup>
 
-
-  <!--popup --> 
-  <!--popup --> 
-
-    <!-- component -->
-      <div class="flex-col w-full  p-10% ">
-    <!-- component -->
-      <div class="flex-col w-full  p-10% ">
-
-
-
-
-
-
-        
-        
-        <div class="flex flex-row">
-         
+    <div class="flex-col w-full  p-10% ">
+      <div class="flex flex-row">
         <div class="flex-none xl:w-1/3 lg:w-1/4 bg-[#027BCE] bg-opacity-10 h-full h-screen overflow-auto overflow-x-hidden">
           <div class=" flex items-stretch	 flex-col h-full lg:space-y-4 md:space-y-2">
             <div class="flex-1/6 justify-left items-center relative md:top-5 lg:left-12 md:left-3">
@@ -290,15 +312,12 @@
                 <div class="object-cover w-full"> 
                   <Datepicker ref="datepicker" @selected-range="handleSelectedRange($event)"/> 
                 </div>
-                </div>
+              </div>
                 
-    
               <div class="flex-col justify-left items-center h-max space-y-3 lg:w-5/6 md:w-2/2 ">
                 <div class="lg:text-2xl md:text-xl ">Lieu</div>
                 <div class="object-cover w-full">
                   <autocomplete_liste :items="cities" ref="lieuAajoute"/>
-                
-                                  
                 </div>
               </div>
               
@@ -320,23 +339,16 @@
               </div>
 
               <div class=" w-full  flex justify-items-start items-start">
-              <description titre="Retranscription" class="w-full min-h-80 " ref="retranscription" />
-              <div class="grid-span-5 border border-black "></div>
+                <description titre="Retranscription" class="w-full min-h-80 " ref="retranscription" />
+                <div class="grid-span-5 border border-black "></div>
               </div>
 
-                <div class="flex justify-center items-center h-min space-y-9 border relative grid-span-5 w-full ">
-              <button @click="flipAnnuler()" type="button" class="relative top-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                <div class="flex justify-center items-center h-min space-y-9 border relative grid-span-5 w-full ">
-              <button @click="flipAnnuler()" type="button" class="relative top-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                <p class="text-xl">Annuler</p>
-              </button>
-                </div>
-              
-              
-
+              <div class="flex justify-center items-center h-min space-y-9 border relative grid-span-5 w-full ">
+                <button @click="flipAnnuler()" type="button" class="relative top-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                  <p class="text-xl">Annuler</p>
+                </button>
+              </div>
             </div>
-          </div>
-        </div>
           </div>
         </div>
 
@@ -350,35 +362,59 @@
                 <div class="flex-row place-content-between  flex  w-5/6 mx-auto">
 
                   <div class=" place-content-start items-start flex w-2/4 ">
-                    <button @click="uploadDocument('tbr')" type="button" class="relative top-1 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-100 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-500 focus:outline-none dark:focus:ring-blue-600">
-                      <p class="text-xl">Enregistrer et ajouter "à Vérifier"</p>
+                    <button @click="updateDocument('tbr')" type="button" class="relative top-1 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-100 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-500 focus:outline-none dark:focus:ring-blue-600">
+                      <p class="text-xl">Enregistrer les modifications</p>
                     </button>
                   </div>
                   <div class=" place-content-start items-start flex w-2/4 ">
-                    <button @click="uploadDocument('tbr')" type="button" class="relative top-1 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-100 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-500 focus:outline-none dark:focus:ring-blue-600">
-                      <p class="text-xl">Enregistrer et ajouter "à Vérifier"</p>
+                    <button @click="updateDocument('tbr')" type="button" class="relative top-1 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-100 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-500 focus:outline-none dark:focus:ring-blue-600">
+                      <p class="text-xl">Enregistrer les modifications</p>
                     </button>
                   </div>
 
                   <div class="place-content-end	flex w-2/4 ">
                     <div>
-                      <button @click="uploadDocument('ntbr')" type="button" class="relative top-1  w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                        <p class="text-xl">Ajouter le  Document</p>
+                      <button @click="transferDocumentById()" type="button" class="relative top-1  w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        <p class="text-xl">Marquer comme vérifié</p>
                       </button>
                     </div>
+                  </div>
+
+                  <div class=" place-content-start items-start flex w-2/4 ">
+                    <button @click="deleteDocument()" type="button" class="relative top-1 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-red-100 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-red-400 dark:hover:bg-red-500 focus:outline-none dark:focus:ring-red-600">
+                      <p class="text-xl">Supprimer</p>
+                    </button>
+                  </div>
+
+
+                  <div class=" place-content-start items-start flex w-2/4 ">
+                    <button @click="downloadDocument()" type="button" class="relative top-1 text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-100 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-green-400 dark:hover:bg-green-500 focus:outline-none dark:focus:ring-green-600">
+                      <p class="text-xl">Télécharger</p>
+                    </button>
                   </div>
                 </div>
                   <div class="place-content-end	flex w-2/4 ">
                     <div>
-                      <button @click="uploadDocument('ntbr')" type="button" class="relative top-1  w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                        <p class="text-xl">Ajouter le  Document</p>
+                      <button @click="transferDocumentById()" type="button" class="relative top-1  w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        <p class="text-xl">Marquer comme vérifié</p>
                       </button>
                     </div>
                   </div>
+
+                  <div class=" place-content-start items-start flex w-2/4 ">
+                    <button @click="deleteDocument()" type="button" class="relative top-1 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-red-100 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-red-400 dark:hover:bg-red-500 focus:outline-none dark:focus:ring-red-600">
+                      <p class="text-xl">Supprimer</p>
+                    </button>
+                  </div>
+
+
+                  <div class=" place-content-start items-start flex w-2/4 ">
+                    <button @click="downloadDocument()" type="button" class="relative top-1 text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-100 font-medium rounded-lg text-base px-10 py-3 me-2 mb-2 dark:bg-green-400 dark:hover:bg-green-500 focus:outline-none dark:focus:ring-green-600">
+                      <p class="text-xl">Télécharger</p>
+                    </button>
+                  </div>
                 </div>
             </div>
-              
-        </div>   
               
         </div>   
     </div>
