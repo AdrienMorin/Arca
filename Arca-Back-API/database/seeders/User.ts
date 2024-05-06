40ac5cb1836fdeb3066c7aac9b99e5f4d3add953
Laziza Bellahbib <aziza.bellahbib@insa-lyon.fr>
2024-05-06 16:55:13 +0200

    peuplerBD

--- Arca-Back-API/database/seeders/User.ts
+++ Arca-Back-API/database/seeders/User.ts
@@ -3,14 +3,35 @@ import User from "App/Models/User";
 
 export default class extends BaseSeeder {
   public async run () {
-    await User.create(
+    await User.createMany([
     {
       "firstname": "Super",
       "lastname": "User",
       "email": "superuser@gmail.com",
       "password": "superuser",
       "role": "superuser"
-    }
-    )
+    },
+    {
+      "firstname": "Christiane",
+      "lastname": "Coste",
+      "email": "christiane.coste@gmail.com",
+      "password": "christianecoste1944",
+      "role": "admin"
+    },
+    {
+      "firstname": "Jean-Marc",
+      "lastname": "Sarnin",
+      "email": "jean-marc.sarnin@gmail.com",
+      "password": "jeanmarcsarnin1944",
+      "role": "admin"
+    },
+    {
+      "firstname": "Françoise",
+      "lastname": "Desloges",
+      "email": "francoise.desloges@gmail.com",
+      "password": "francoisedesloges1944",
+      "role": "user"
+    },
+    ])
   }
 }
